ecf4117680ab718f72d5e944b9863729167c6871
irteam sudo-account <irteamsu@gpuapp-svc003.svr.toastmaker.net>
2017-06-29 10:35:06 +0900

    Define functions for image loading and feedforwarding

--- single_feedforward_test.py
+++ single_feedforward_test.py
@@ -18,20 +18,24 @@ print 'Single Feedforward Test Started...'
 parser = argparse.ArgumentParser()
 opt = TestOptions().parse()
 opt.nThreads = 1   # test code only supports nThreads = 1
-opt.batchSize = 1  # test code only supports batchSize = 1
-opt.serial_batches = True  # no shuffle
-opt.no_flip = True  # no flip
-opt.name = 'maps_cyclegan'
-opt.model = 'test'
-opt.dataset_mode = 'single'
-opt.phase = 'test'
+#opt.batchSize = 1  # test code only supports batchSize = 1
+#opt.serial_batches = True  # no shuffle
+#opt.no_flip = True  # no flip
+#opt.name = 'anonymous'
+#opt.model = 'test'
+#opt.dataset_mode = 'single'
+#opt.phase = 'test'
 
-result_dir = "/media/illusion/ML_DATA_SSD_M550/pytorch-CycleGAN-and-pix2pix/temp/"
-model_path = "/media/illusion/ML_DATA_SSD_M550/pytorch-CycleGAN-and-pix2pix/checkpoints/maps_cyclegan/1_net_G_A.pth"
+result_dir = "/home1/irteamsu/users/rklee/gan/pytorch-CycleGAN-and-pix2pix-modified/temp/"
+model_path = "/home1/irteamsu/data_ssd/users/rklee/gan/pytorch-CycleGAN-and-pix2pix-modified/checkpoints_for_demo/latest_net_G_A_black_to_blonde.pth"
+
+# Load an image
+#A_path = "/home/illusion/ML_Linux_SSD_M550/pytorch-CycleGAN-and-pix2pix/datasets/maps/test/736_A.jpg"
+A_path = "/home1/irteamsu/data_ssd/users/rklee/gan/pytorch-CycleGAN-and-pix2pix-modified/temp/result_2017_04_09_22_42_40_187477_97062.jpg"
 
 # Create the model
-model = TestModel()
-model.initialize(opt)
+#model = TestModel()
+#model.initialize(opt)
 
 assert (not opt.isTrain)
 
@@ -43,18 +47,36 @@ gpu_ids.append(0)
 tensor = torch.cuda.FloatTensor if gpu_ids else torch.Tensor
 input_A = tensor(opt.batchSize, opt.input_nc, opt.fineSize, opt.fineSize)
 
+def insert_image(image_full_path):
+    
+    global input_A
+    
+    A_img = Image.open(image_full_path).convert('RGB')
+    A_img = transform(A_img)
+    A_img_reshaped = A_img.view(-1, 3, 256, 256)
+
+    input_A.resize_(A_img_reshaped.size()).copy_(A_img_reshaped)
+    
+def forward_a_test_image():
+    real_A = Variable(input_A)
+    fake_B = netG.forward(real_A)
+
+    real_A_im = util.tensor2im(real_A.data)
+    fake_B_im = util.tensor2im(fake_B.data)
+    visuals = OrderedDict([('real_A', real_A_im), ('fake_B', fake_B_im)])
+    return visuals
+
+# Load the generator model
 netG = networks.define_G(opt.input_nc, opt.output_nc,
                               opt.ngf, opt.which_model_netG,
                               opt.norm, opt.use_dropout,
                               gpu_ids)
 
-print('---------- Networks initialized -------------')
-networks.print_network(netG)
-print('-----------------------------------------------')
-
 netG.load_state_dict(torch.load(model_path))
 
-visualizer = Visualizer(opt)
+print('---------- Networks initialized and loaded -------------')
+networks.print_network(netG)
+print('--------------------------------------------------------')
 
 # Data related preparation
 transform_list = []
@@ -73,30 +95,13 @@ transform_list += [transforms.ToTensor(),
 
 transform = transforms.Compose(transform_list)
 
-# Load an image
-#A_path = "/home/illusion/ML_Linux_SSD_M550/pytorch-CycleGAN-and-pix2pix/datasets/maps/test/736_A.jpg"
-A_path = "./datasets/maps/test/1000_A.jpg"
-
-A_img = Image.open(A_path).convert('RGB')
-A_img = transform(A_img)
-A_img_reshaped = A_img.view(-1, 3, 256, 256)
-
-data = {'A': A_img_reshaped, 'A_paths': A_path}
-
-# we need to use single_dataset mode
-input_A_temp = data['A']
-input_A.resize_(input_A_temp.size()).copy_(input_A_temp)
-image_paths = data['A_paths']
+# Insert a test image
+insert_image(A_path)
 
 # Feedforward a test image
-real_A = Variable(input_A)
-fake_B = netG.forward(real_A)
+visuals = forward_a_test_image()
 
-#visuals = model.get_current_visuals()
-real_A_im = util.tensor2im(real_A.data)
-fake_B_im = util.tensor2im(fake_B.data)
-visuals = OrderedDict([('real_A', real_A_im), ('fake_B', fake_B_im)])
-
+# Save images
 for label, image_numpy in visuals.items():
     image_name = '%s_%s.png' % ("result", label)
     save_path = os.path.join(result_dir, image_name)
